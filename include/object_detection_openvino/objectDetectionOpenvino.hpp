1b7de854497b4a4ddd82e38a534d31409b5fab56
Alberto Tudela <ajtudela@gmail.com>
2021-05-28 12:22:08 +0200

    Remove depth topic

--- include/object_detection_openvino/objectDetectionOpenvino.hpp
+++ include/object_detection_openvino/objectDetectionOpenvino.hpp
@@ -1,7 +1,7 @@
 /*
  * OBJECT DETECTION OPENVINO ROS NODE
  *
- * Copyright (c) 2020 Alberto José Tudela Roldán <ajtudela@gmail.com>
+ * Copyright (c) 2020-2021 Alberto José Tudela Roldán <ajtudela@gmail.com>
  * 
  * This file is part of object_detection_openvino project.
  * 
@@ -32,20 +32,17 @@
 #include <std_msgs/Header.h>
 #include <std_msgs/Int8.h>
 #include <geometry_msgs/Pose.h>
+#include <geometry_msgs/Vector3.h>
 #include <sensor_msgs/image_encodings.h>
 #include <sensor_msgs/Image.h>
 #include <sensor_msgs/RegionOfInterest.h>
+#include <sensor_msgs/CameraInfo.h>
 #include <image_transport/image_transport.h>
-#include <image_transport/subscriber_filter.h>
-#include <message_filters/subscriber.h>
-#include <message_filters/synchronizer.h>
-#include <message_filters/sync_policies/approximate_time.h>
-#include <visualization_msgs/MarkerArray.h>
+
 #include <object_detection_openvino/yoloParams.hpp>
 #include <object_detection_openvino/Detection2DArray.h>
-#include <object_detection_openvino/Detection3DArray.h>
 
-// OpenCv
+// OpenCV
 #include <samples/slog.hpp>
 #include <cv_bridge/cv_bridge.h>
 #include <opencv2/core/core.hpp>
@@ -85,28 +82,29 @@ class ObjectDetectionOpenvino{
 			bool operator<(const DetectionObject &s2) const;
 			bool operator>(const DetectionObject &s2) const;
 			object_detection_openvino::Detection2D Detection2D(int id);
-			object_detection_openvino::Detection3D Detection3D(int id);
 		};
 
+	private:
+		// ROS related
 		ros::NodeHandle node_, nodePrivate_;
 		ros::ServiceServer paramsSrv_;
 		image_transport::ImageTransport imageTransport_;
-		image_transport::SubscriberFilter imageSubscriberFilter_, depthSubscriberFilter_;
-		message_filters::Subscriber<sensor_msgs::CameraInfo> cameraInfoSubscriber_;
-		typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::Image, sensor_msgs::Image, sensor_msgs::CameraInfo> MySyncPolicy;
-		message_filters::Synchronizer<MySyncPolicy> sync;
-		ros::Publisher detection2DPublisher_, markerPublisher_, detection3DPublisher_;
-		image_transport::Publisher detectionImagePublisher_;
+		image_transport::Subscriber colorSub_;
+		image_transport::Publisher detectionColorPub_;
+		ros::Subscriber infoSub_;
+		ros::Publisher detection2DPub_;
+
 
 		std::string inputName_, networkType_;
 		std::string modelFileName_, binFileName_, labelFileName_;
-		std::string colorFrameId_, depthFrameId_, infoTopic_, colorTopic_, depthTopic_, imageDetectedTopic_, detection2DTopic_, detection3DTopic_, deviceTarget_;
+		std::string colorFrameId_, infoTopic_, colorTopic_,  detectionImageTopic_, detection2DTopic_, deviceTarget_;
 		std::vector<std::string> labels_;
-		
+
 		int detectionId_;
-		float thresh_, iouThresh_;
-		bool showFPS_, outputImage_, outputMarkers_;
-		
+		float fx_, fy_, cx_, cy_;
+		bool showFPS_, outputImage_;
+
+		// OpenVino related
 		std::map<std::string, YoloParams> yoloParams_;
 		cv::Mat nextFrame_, currFrame_, depthFrame_;
 		InferenceEngine::InferRequest::Ptr async_infer_request_curr_, async_infer_request_next_;
@@ -114,20 +112,24 @@ class ObjectDetectionOpenvino{
 		InferenceEngine::InputsDataMap inputInfo_;
 		InferenceEngine::CNNNetwork cnnNetwork_;
 		InferenceEngine::Core core_;
+		float thresh_, iouThresh_;
 
-		typedef std::chrono::duration<double, std::ratio<1, 1000>> ms;
-		
-		void initialize() { std_srvs::Empty empt; updateParams(empt.request, empt.response); }
-		bool updateParams(std_srvs::Empty::Request& req, std_srvs::Empty::Response& res);
-		void frameToBlob(const cv::Mat &frame, InferenceEngine::InferRequest::Ptr &inferRequest, const std::string &inputName, bool autoResize = false);
 		int getColor(int c, int x, int max);
 		static int entryIndex(int side, int lcoords, int lclasses, int location, int entry);
 		double intersectionOverUnion(const DetectionObject &box_1, const DetectionObject &box_2);
-		void parseYOLOV3Output(const YoloParams &params, const std::string &outputName, const InferenceEngine::Blob::Ptr &blob, const unsigned long resizedImgH, const unsigned long resizedImgW, const unsigned long originalImgH, const unsigned long originalImgW, const float threshold, std::vector<DetectionObject> &objects);
+		void frameToBlob(const cv::Mat &frame, InferenceEngine::InferRequest::Ptr &inferRequest, const std::string &inputName, bool autoResize = false);
 		void parseSSDOutput(const InferenceEngine::CNNLayerPtr &layer, const InferenceEngine::Blob::Ptr &blob, const unsigned long height, const unsigned long width, const float threshold, std::vector<DetectionObject> &objects);
-		void cameraCallback(const sensor_msgs::ImageConstPtr& colorImageMsg, const sensor_msgs::ImageConstPtr& depthImageMsg, const sensor_msgs::CameraInfo::ConstPtr& infoMsg);
-		visualization_msgs::Marker createBoundingBox3dMarker(int id, geometry_msgs::Pose center, geometry_msgs::Vector3 size, float colorRGB[3], std::string targetFrame, ros::Time stamp);
-		visualization_msgs::Marker createLabel3dMarker(int id, std::string label, geometry_msgs::Pose pose, float colorRGB[3], std::string targetFrame, ros::Time stamp);
+		void parseYOLOV3Output(const YoloParams &params, const std::string &outputName, const InferenceEngine::Blob::Ptr &blob, const unsigned long resizedImgH, const unsigned long resizedImgW, const unsigned long originalImgH, const unsigned long originalImgW, const float threshold, std::vector<DetectionObject> &objects);
+
+		
+		
+
+		typedef std::chrono::duration<double, std::ratio<1, 1000>> ms;
+
+		void initialize() { std_srvs::Empty empt; updateParams(empt.request, empt.response); }
+		bool updateParams(std_srvs::Empty::Request& req, std_srvs::Empty::Response& res);
+		void infoCallback(const sensor_msgs::CameraInfo::ConstPtr& infoMsg);
+		void cameraCallback(const sensor_msgs::ImageConstPtr& colorImageMsg);
 		void publishImage(cv::Mat image);
 		void publishBoundingImage(cv::Mat image);
 };
--- include/object_detection_openvino/yoloParams.hpp
+++ include/object_detection_openvino/yoloParams.hpp
@@ -1,7 +1,7 @@
 /*
  * YOLO PARAMS
  *
- * Copyright (c) 2020 Alberto José Tudela Roldán <ajtudela@gmail.com>
+ * Copyright (c) 2020-2021 Alberto José Tudela Roldán <ajtudela@gmail.com>
  * 
  * This file is part of object_detection_openvino project.
  * 
@@ -16,9 +16,9 @@
 #include <inference_engine.hpp>
 #include <ngraph/ngraph.hpp>
 
-class YoloParams {
+class YoloParams{
 		template <typename T>
-		void computeAnchors(const std::vector<T> & mask) {
+		void computeAnchors(const std::vector<T> & mask){
 			std::vector<float> maskedAnchors(num * 2);
 			for (int i = 0; i < num; ++i) {
 				maskedAnchors[i * 2] = anchors[mask[i] * 2];
@@ -31,9 +31,9 @@ class YoloParams {
 		int num = 0, classes = 0, coords = 0;
 		std::vector<float> anchors = {10.0, 13.0, 16.0, 30.0, 33.0, 23.0, 30.0, 61.0, 62.0, 45.0, 59.0, 119.0, 116.0, 90.0, 156.0, 198.0, 373.0, 326.0};
 
-		YoloParams() {}
+		YoloParams(){}
 
-		YoloParams(const std::shared_ptr<ngraph::op::RegionYolo> regionYolo) {
+		YoloParams(const std::shared_ptr<ngraph::op::RegionYolo> regionYolo){
 			coords = regionYolo->get_num_coords();
 			classes = regionYolo->get_num_classes();
 			anchors = regionYolo->get_anchors();
