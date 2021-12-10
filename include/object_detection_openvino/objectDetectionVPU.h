250552394e559aa0eb6717c29f663cef40617508
Alberto Tudela <ajtudela@gmail.com>
2021-12-10 12:37:33 +0100

    Initial pcl

--- include/object_detection_openvino/objectDetectionVPU.h
+++ include/object_detection_openvino/objectDetectionVPU.h
@@ -17,15 +17,21 @@
 #include <string>
 #include <vector>
 
+// PCL
+#include <pcl/point_types.h>
+#include <pcl/point_cloud.h>
+
 // ROS
 #include <ros/ros.h>
 #include <std_msgs/Header.h>
+#include <tf/transform_listener.h>
 #include <geometry_msgs/Pose.h>
 #include <geometry_msgs/Vector3.h>
 #include <sensor_msgs/Image.h>
-#include <sensor_msgs/CameraInfo.h>
+#include <sensor_msgs/PointCloud2.h>
 #include <image_transport/image_transport.h>
 #include <image_transport/subscriber_filter.h>
+#include <message_filters/subscriber.h>
 #include <message_filters/synchronizer.h>
 #include <message_filters/sync_policies/approximate_time.h>
 #include <visualization_msgs/Marker.h>
@@ -40,7 +46,9 @@
 
 #define COCO_CLASSES		80
 
+// Typedef for easier readability
 typedef std::chrono::duration<double, std::ratio<1, 1000>> ms;
+typedef pcl::PointCloud<pcl::PointXYZRGB> pcloud;
 
 class ObjectDetectionVPU{
 	public:
@@ -50,33 +58,30 @@ class ObjectDetectionVPU{
 	private:
 		ros::NodeHandle node_, nodePrivate_;
 		image_transport::ImageTransport imageTransport_;
-		image_transport::SubscriberFilter colorSub_, depthSub_;
+		image_transport::SubscriberFilter colorSub_;
 		image_transport::Publisher detectionColorPub_;
-		ros::Subscriber depthInfoSub_;
+		message_filters::Subscriber<sensor_msgs::PointCloud2> pointsSub_;
 		ros::Publisher detectionInfoPub_, detectionsPub_, markersPub_;
-
-		typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::Image, sensor_msgs::Image> SyncPolicyTwoImage;
-		message_filters::Synchronizer<SyncPolicyTwoImage> syncTwoImage_;
+		tf::TransformListener tfListener_;
+		typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::Image, sensor_msgs::PointCloud2> SyncPolicyImagePCL;
+		message_filters::Synchronizer<SyncPolicyImagePCL> syncImagePCL_;
 
 		Openvino openvino_;
-		float fx_, fy_, cx_, cy_;
 		float thresh_, iouThresh_;
 		bool showFPS_, useDepth_, outputImage_;
 		std::string deviceTarget_, networkType_;
 		std::string modelFileName_, binFileName_, labelFileName_;
-		std::string colorFrameId_, colorTopic_, depthInfoTopic_, depthTopic_, detectionImageTopic_, detectionInfoTopic_, detectionsTopic_;
+		std::string colorFrameId_, colorTopic_, pointCloudTopic_, detectionImageTopic_, detectionInfoTopic_, detectionsTopic_;
 		cv::Mat nextFrame_, currFrame_;
 		std::vector<std::string> labels_;
 
 		void getParams();
 		int getColor(int c, int x, int max);
-		void depthInfoCallback(const sensor_msgs::CameraInfo::ConstPtr& infoMsg);
-		void oneImageCallback(sensor_msgs::Image::ConstPtr colorImageMsg);
-		void twoImageCallback(sensor_msgs::Image::ConstPtr colorImageMsg, sensor_msgs::Image::ConstPtr depthImageMsg);
-		void cameraCallback(const std::vector<sensor_msgs::Image::ConstPtr>& imageMsg);
+		void colorImageCallback(sensor_msgs::Image::ConstPtr colorImageMsg);
+		void colorPointCallback(sensor_msgs::Image::ConstPtr colorImageMsg, sensor_msgs::PointCloud2::ConstPtr pointsMsg);
 		void showHistogram(cv::Mat image, cv::Scalar mean);
 		vision_msgs::Detection2D createDetection2DMsg(DetectionObject object, std_msgs::Header header);
-		vision_msgs::Detection3D createDetection3DMsg(cv_bridge::CvImagePtr depthImage, DetectionObject object, std_msgs::Header header);
+		vision_msgs::Detection3D createDetection3DMsg(sensor_msgs::PointCloud2 cloudPC2, pcloud::ConstPtr cloudPCL, DetectionObject object, std_msgs::Header header);
 		visualization_msgs::Marker createBBox3dMarker(int id, geometry_msgs::Pose center, geometry_msgs::Vector3 size, float colorRGB[3], std_msgs::Header header);
 		visualization_msgs::Marker createLabel3dMarker(int id, std::string label, geometry_msgs::Pose pose, float colorRGB[3], std_msgs::Header header);
 		void publishImage(cv::Mat image);
